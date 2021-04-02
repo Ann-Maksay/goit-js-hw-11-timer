59721eaad39a61ed2a668c182a6ea95f615abc82
Ann Maksay <annaorlova.dn@gmail.com>
2021-04-02 21:21:38 +0300

    Готовая работа

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+class CountdownTimer{
+  constructor(obj) {
+    this.selector = obj.selector;
+    this.targetDate = obj.targetDate;
+  }
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  _intervalId = null;
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  _spanRefs = {
+    daysRef: document.querySelector('span[data-value="days"]'),
+    hoursRef: document.querySelector('span[data-value="hours"]'),
+    minsRef: document.querySelector('span[data-value="mins"]'),
+    secsRef: document.querySelector('span[data-value="secs"]')
+  }
+  
+  start() {
+    const targetTime = this.targetDate.getTime();
 
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+    this._updateClock(0);
+
+    this._intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = targetTime - currentTime;
+
+      if (deltaTime <= 0) {
+        this.stop();
+      }
+
+      this._updateClock(deltaTime);
+
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this._intervalId);
+    this._intervalId = null;
+    this._updateClock(0);
+  }
+
+  _updateClock(time) {
+    this._spanRefs.daysRef.textContent = `${this._pad(Math.floor(time / (1000 * 60 * 60 * 24)))}`
+    this._spanRefs.hoursRef.textContent = `${this._pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))}`
+    this._spanRefs.minsRef.textContent = `${this._pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)))}`
+    this._spanRefs.secsRef.textContent = `${this._pad(Math.floor((time % (1000 * 60)) / 1000))}`
+  }
+
+  _pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
 
 
-new CountdownTimer({
+
+
+const testTimer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+  targetDate: new Date('Apr 25, 2021'),
 });
+
+testTimer.start();
